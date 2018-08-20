c6f0aeb31fb195347b0c398864afefa63e4c5ee9
mjr390 <mjr117@yahoo.com>
2018-08-20 19:16:31 -0400

    added to code to append rows to a table from the data, also code to grab the value in the input section

--- static/js/app.js
+++ static/js/app.js
@@ -1,2 +1,23 @@
 // from data.js
 var tableData = data;
+var tbody = d3.select("tbody");
+
+data.forEach((siteReport) => {
+  var row = tbody.append("tr");
+  Object.entries(siteReport).forEach(([key, value]) => {
+    var cell = tbody.append("td");
+    cell.text(value);
+  });
+});
+
+var button = d3.select("#filter-btn");
+
+var inputField = d3.select("#datetime");
+// console.log(inputField.html);
+button.on("click", function() {
+  d3.event.preventDefault();
+  console.log("Hi, a button was clicked!");
+  var inputValue = inputField.property("value");
+
+  console.log(inputValue);
+});
