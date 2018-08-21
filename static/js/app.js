f7ab19e0044ec1bd8c6897f65e12f3d5453d5173
mjr390 <mjr117@yahoo.com>
2018-08-20 22:09:57 -0400

    added a filter to return only data for a selected date

--- static/js/app.js
+++ static/js/app.js
@@ -16,8 +16,29 @@ var inputField = d3.select("#datetime");
 // console.log(inputField.html);
 button.on("click", function() {
   d3.event.preventDefault();
+  d3.select("tbody").text("");
   console.log("Hi, a button was clicked!");
   var inputValue = inputField.property("value");
 
   console.log(inputValue);
+
+  // Create a custom filtering function
+  function selectRequested(date) {
+    return date.datetime === inputValue;
+  }
+
+  // filter() uses the custom function as its argument
+  var requested = data.filter(selectRequested);
+  console.log(requested)
+
+  requested.forEach((siteReport) => {
+    var row = tbody.append("tr");
+    Object.entries(siteReport).forEach(([key, value]) => {
+      var cell = tbody.append("td");
+      cell.text(value);
+    });
+  });
+
+  // var mydate = new Date(inputValue);
+  // console.log(mydate.toDateString())
 });
